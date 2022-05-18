33c34cc127d24692458829f2ba0fe4a397de21e6
IT21265242Zimmendra <99347087+IT21265242Zimmendra@users.noreply.github.com>
2022-05-19 00:26:16 +0530

    login

---
+++ php/login.php
@@ -0,0 +1,34 @@
+<?php
+    require('config.php');
+    require('cid.php');
+
+    session_start();
+
+    if(isset($_POST["uname"]) && isset($_POST["pword"])){
+        $gname = $_POST["uname"];
+        $pword = $_POST["pword"];
+        
+        $sql = "SELECT * from users WHERE Username = '$uname'  AND password  = '$pword' " ;
+        $result = $con - > query($sql);
+        if(num_rows($result)===1){
+            
+            $row = fetch_assoc($result);
+
+            if($row['Username'] === $uname && $row ['Password'] === $psword){
+                echo "You have successfully logged in";
+
+                $_SESSION['Username'] = $row['Username'];
+                $_SESSION['Type'] = $row['Type'];
+
+                header("location:");
+                exit();
+            }
+            else{
+                echo "Incorrect user name or password";
+                exit();
+            }
+
+        }
+
+    }
+?>

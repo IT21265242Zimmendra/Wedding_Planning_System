0bef1e830e1a56f7c8f3189dd42941f905fec740
Ashvini09 <99312058+IT21276446@users.noreply.github.com>
2022-05-20 15:23:56 +0530

    changed header/footer

--- php/feedback.php
+++ php/feedback.php
@@ -10,30 +10,29 @@
  <title> Feedback | The Wedding Table </title>
  </head>
  <body>
-     <!-- Nav bar-->
-     <header>
-        <div class="header">
-            <div class = "container1">
-                <div class="navbar">
-                    <div class = "logo1">
-                        <img src = "../images/logo.png" width = "125px">
-                    </div>
-                    <nav>
-                        <ul>
-                            <li> <a href="#" class="active1">Home</a></li>
-                            <li> <a href="#"class="active1">Categories</a></li>
-                            <li> <a href="#" class="active1">About Us</a></li>
-                        </ul>
-                    </nav>
-                        <div class="sign-in">
-                            <button class="visitorbtn">Sign Up</button>
-                        </div>
-                </div> 
-
-                </div>
+      <!------------------------------Header------------------------------------>
+      <div class="navbar">
+            <div class = "logo1">
+                <img src = "./../IMAGES/logo.png" width = "125px">
             </div>
-        </div>
-    </header>
+            <nav>
+                <ul>
+                    <li> <a href="#" class="active1">Home</a></li>
+                    <li> <a href="#"class="active1">Categories</a></li>
+                    <li> <a href="#" class="active1">About Us</a></li>
+                </ul>
+            </nav>
+            <div class="sign-in">
+              <button class="signinbtn"><img src = "./../IMAGES/pro.png"> &nbsp Sign In</button>
+              <div class="sign-in-content">
+                <ul>
+                    <li><a href="#">Log Out</a></li>
+                    <li><a href="#">Settings</a></li>
+                    <li><a href="#">Dashboard</a></li>
+                </ul>
+              </div>
+            </div>
+        </div> 
 <!---------------------------------------------------------------------------------------->
 
 <div class="addfdbk">
@@ -46,9 +45,9 @@
  <div class = "feedbacktbl">
      <form method="get" action = "#">
      <?php
-        $cusId =  $_SESSION['Username'];
+        /*$cusId =  $_SESSION['Username'];*/
         $sql = "SELECT*
-                FROM feedback WHERE Cid = $cusId";
+                FROM feedback WHERE Cid = '1'/*$cusId*/";
         $result = $con->query($sql);
         
         if($result -> num_rows >0){
@@ -76,6 +75,26 @@
  </div>
 
 </div>
+
+<!-----------------------------------------Footer---------------------------->
+
+<footer>
+
+<div class="footer">
+        
+    <img src="./../IMAGES/logo.png" class="logo2" height="15%" width="10%">
+        <img src="./../IMAGES/play-store.png" class="bind1" height="10%" width="10%">
+        <img src="./../IMAGES/app-store.png" class="bind1" height="10%" width="10%">
+        <img src="./../IMAGES/Contact us.png" class="bind1" height="10%" width="10%">
+        
+
+    <img src="./../IMAGES/fb.png" class="bind2"  width="3%">
+    <img src="./../IMAGES/inster.png" class="bind2" width="3%">
+    <img src="./../IMAGES/tw.png" class="bind2" width="3%">
+    
+</div>
+
+</footer>
  
  
  </body>

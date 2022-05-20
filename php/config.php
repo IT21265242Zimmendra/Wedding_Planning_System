25a3e81c7cad5e8331c53b74761ac076424ba0fe
Ashvini09 <99312058+IT21276446@users.noreply.github.com>
2022-05-20 14:07:49 +0530

    config.php

--- php/config.php
+++ php/config.php
@@ -3,7 +3,7 @@
     $server = "localhost";
     $username = "root";
     $password = "";
-    $database = "weddingplanningsystem";
+    $database = "wedding_planing_system";
 
     $con = new mysqli($server, $username, $password, $database);
 

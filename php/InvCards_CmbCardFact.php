057e7a7c8768fa3c9acdde47352ff08757c4bfd1
IT21294952 <99532670+IT21294952@users.noreply.github.com>
2022-05-22 18:00:44 +0530

    Inv card vendors

--- php/InvCards_CmbCardFact.php
+++ php/InvCards_CmbCardFact.php
@@ -38,15 +38,15 @@
         <!-------------------------------------------------Body------------------------------------------------->
 
         <form>
-            <h1>Anim8 (Pvt.) Ltd.</h1>
+            <h1>Colombo Card Factory</h1>
         
         <div class="layout">
             <div id="slider"> <!--------------Slideshow----------->
                 <figure>
-                    <img src="../images/IC006.jpg">
-                    <img src="../images/IC007.jpg">
-                    <img src="../images/IC008.jpg">
-                    <img src="../images/IC003.jpg">
+                    <img src="../images/IC0024.jpg">
+                    <img src="../images/IC0025.jpg">
+                    <img src="../images/IC0026.jpg">
+                    <img src="../images/IC0027.jpg">
                 </figure>
 
                 <br><br>
@@ -99,13 +99,15 @@
             <div class="desc">
                 <br><br>
                 <h3>Description</h3>
-                <p>If you didn't already know, we have our very own brand of stationery for all your wedding needs. And while we might be just a little bit biased here, there are so many great reasons to choose our brand for your big day. So what makes ours the best place to order your wedding invitations online? <br>Let us fill you in.</p>
+                <p>Luxurious Wedding Invitations by Colombo Card Factory 💗
+Invite your loved ones with our elegant cards and blossom your best day! 
+We make your dreams reality!!</p>
                 
                 <br><br>
                 <h3>Location</h3>
                 <p>
                     <br>
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3960.9796411580614!2d79.86759881464529!3d6.8930383206696!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae25bd3f10b668d%3A0x27e34c43ed4171c5!2sAnim8%20(Pvt)%20Ltd!5e0!3m2!1sen!2slk!4v1652100832054!5m2!1sen!2slk" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.5480407671735!2d79.91254551464513!3d6.824689421482297!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2507d78f25d3b%3A0xc256d5bee3f713d7!2sColombo%20Card%20Factory!5e0!3m2!1sen!2slk!4v1653221866747!5m2!1sen!2slk" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </p>
 
                 <br><br><br><br><br>
